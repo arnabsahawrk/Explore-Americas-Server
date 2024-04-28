367ffa0732821c23982893d50944943db3631ec0
arnabw <arnabsahawrk@gmail.com>
2024-04-28 17:31:15 +0600

    connect with mongodb and intigrate post method

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 //config
 require("dotenv").config();
@@ -11,7 +12,34 @@ app.use(cors());
 app.use(express.json());
 
 //Database Authenticate
-const uri = "mongodb://localhost:27017";
+// const uri = "mongodb://localhost:27017";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2p5zaxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    const exploreAmericasDB = client
+      .db("explore-americas")
+      .collection("tourist-spots");
+
+    //POST
+    app.post("/tourist-spots", async (req, res) => {
+      const insertedResult = await exploreAmericasDB.insertOne(req.body);
+      res.send(insertedResult);
+    });
+  } catch (err) {
+    console.log("Fell Database.", err);
+  }
+};
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send(`Server is running on ${port}`);
